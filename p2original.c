33b055c3ecf925699e49504b1fb3c523c6b69b46
Sanvi Sundarrajan <93093295+Sanvi-Sundarrajan@users.noreply.github.com>
2022-02-28 15:18:43 +0000

    p1 and debug

---
+++ p2original.c
@@ -0,0 +1,11 @@
+/*2. Write a program to find the smallest of three fractions.
+struct _fraction
+{
+int num,den;
+};
+typedef _fraction Fraction
+Fraction input_fraction();
+Fraction Largest_fraction(Fraction f1, Fraction f2, Fraction f3)
+void output(Fraction f1, Fraction f2, Fraction f3, Fraction largest)
+
+*/
